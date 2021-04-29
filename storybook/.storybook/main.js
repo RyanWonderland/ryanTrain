e4443123881a3240768bd41f17e84d6e96273a37
xjjwonderland <1353225073@qq.com>
2021-04-29 09:12:40 +0800

    this is a story book

---
+++ storybook/.storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+    stories: ['../stories/*.stories.@(ts|tsx|js|jsx|mdx)'],
+    addons: ['@storybook/addon-links', 
+             '@storybook/addon-essentials',
+             './stories/form.stories.js'
+            ],
+  }
